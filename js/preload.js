2d7d2f12da4dc1d05c2f865f4d3f41360d1bb4af
GoldenMelon <goldenmelon401@gmail.com>
2015-09-04 00:33:38 +0300

    old changes that i don't remember

---
+++ js/preload.js
@@ -0,0 +1,12 @@
+/**
+ * Created by goldenmelon on 22.08.15.
+ */
+var arr = new Array(5);
+for(var i = 0; i < arr.length; i++){
+    arr[i] = new Image();
+}
+arr[0].src = "../images/shutterstock_70010998_720.jpg";
+arr[1].src = "../images/shutterstock_92066195_720..jpg";
+arr[2].src = "../images/shutterstock_107922686_720.jpg";
+arr[3].src = "../images/shutterstock_109936955_720.jpg";
+arr[4].src = "../images/shutterstock_153829943_720.jpg";
